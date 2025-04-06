096ef40b5725c4e4f39424ba5d62b30c0685ea38
moak13 <michaelkalango@gmail.com>
2025-04-06 13:25:54 +0100

    feat: app colors

--- lib/ui/common/app_colors.dart
+++ lib/ui/common/app_colors.dart
@@ -7,3 +7,18 @@ const Color kcMediumGrey = Color(0xFF474A54);
 const Color kcLightGrey = Color.fromARGB(255, 187, 187, 187);
 const Color kcVeryLightGrey = Color(0xFFE3E3E3);
 const Color kcBackgroundColor = kcDarkGreyColor;
+
+class AppColors {
+  static const Color kcPrimary = Color(0xff0659FD);
+  static const Color kcPrimaryDark = Color(0xff210FA4);
+  static const Color kcPrimaryLight = Color(0xff5244EF);
+  static const Color kcBlack = Color(0xff121212);
+  static const Color kcBlack100 = Color(0xff161616);
+  static const Color kcBlack200 = Color(0xff191919);
+  static const Color kcBlack300 = Color(0xff333333);
+  static const Color kcGrey = Color(0xffCCCCCC);
+  static const Color kcGrey100 = Color(0xffAAAAAA);
+  static const Color kcAlertDanger = Color(0xffFF0000);
+  static const Color kcAlertSuccess = Color(0xff53D769);
+  static const Color kcAlertWarning = Color(0xffF9B853);
+}
